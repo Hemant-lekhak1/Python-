9c1125d3007da4e03c550b82cd53cf15784bcc3d
Hemant Lekhak <lekhakhemant1@gmail.com>
2025-03-05 10:01:30 +0545

    Variables in Python

---
+++ Variables.py
@@ -0,0 +1,12 @@
+My_name="Hello World"     #My_name is variable that stores Hello World data i.e. string data
+print(My_name)            #To print Data stored inside My_name
+
+roll_no=13                #roll_no is a variable storing a integer data 
+print(roll_no)            #To print Data stored inside roll_no
+
+marks=97.50               #marks is a variable storing a float data 
+print(marks)              #To print Data stored inside marks
+
+"""we cannot use numbers at the beginning of variable name but numbers can be used after alphabet characters in variable names.
+   we should not specify the data type in python so we can simply use variable name only 
+   for example instead of int a= 10   we can simply write a=10"""
